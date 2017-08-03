f23cab30ebcfeac7c44c9fd32e2a2ba20ff89a55
praaveen <praaveen.vr@spritle.com>
2017-08-03 18:39:43 +0530

    ui change

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,17 +1,16 @@
 $( document ).ready(function() {
     $('#submit').click(function () {
-        const client = new ApiAi.ApiAiClient({accessToken: '*******'});
-        var message = $('#message').val()
-        const promise = client.textRequest(message);
+        const client = new ApiAi.ApiAiClient({accessToken: 'fd71e081c5c345d090f8b29f35b81f0f'});
+        var message1 = $('.message_input').val()
+        const promise = client.textRequest(message1);
 
         promise
             .then(handleResponse)
             .catch(handleError);
 
         function handleResponse(serverResponse) {
-            console.log(serverResponse);
-            $("h4").append('</br>' + 'User says : ' + message);
-            $( "p" ).append('</br>' + 'Bot says : ' + serverResponse.result.fulfillment.speech);
+            sendMessage(message1)
+            sendMessage(serverResponse.result.fulfillment.speech);
         }
 
         function handleError(serverError) {
@@ -19,5 +18,45 @@ $( document ).ready(function() {
         }
     });
 
+    var Message;
+    Message = function (arg) {
+        this.text = arg.text, this.message_side = arg.message_side;
+        this.draw = function (_this) {
+            return function () {
+                var $message;
+                $message = $($('.message_template').clone().html());
+                $message.addClass(_this.message_side).find('.text').html(_this.text);
+                $('.messages').append($message);
+                return setTimeout(function () {
+                    return $message.addClass('appeared');
+                }, 0);
+            };
+        }(this);
+        return this;
+    };
+
+        var getMessageText, message_side, sendMessage;
+        message_side = 'right';
+        getMessageText = function () {
+            var $message_input;
+            $message_input = $('.message_input');
+            return $message_input.val();
+        };
+        sendMessage = function (text) {
+            var $messages, message;
+            if (text.trim() === '') {
+                return;
+            }
+            $('.message_input').val('');
+            $messages = $('.messages');
+            message_side = message_side === 'left' ? 'right' : 'left';
+            message = new Message({
+                text: text,
+                message_side: message_side
+            });
+            message.draw();
+            return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+        }
 
 });
+
